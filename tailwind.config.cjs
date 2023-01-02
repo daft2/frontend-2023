0dd2aa1c0220d5a32a954ed001002db145d9d035
Daft <daftdevs@gmail.com>
2023-01-02 17:57:07 +0700

    feat: add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,14 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      colors: {
+        nhpSoftOrange: "hsl(35, 77%, 62%)",
+        nhpSoftRed: "hsl(5, 85%, 63%)",
+        nhpOffWhite: "hsl(36, 100%, 99%)",
+        nhpGrayishBlue: "hsl(233, 8%, 79%)",
+        nhpDarkGrayishBlue: "hsl(236, 13%, 42%)",
+        nhpVeryDarkBlue: "hsl(240, 100%, 5%)",
+      },
     },
   },
   plugins: [],
