e31cdd18993b0b12f964972aa46db7ed5dc8d0f4
Daft <daftdevs@gmail.com>
2023-01-04 23:30:22 +0700

    ref: header mobile & desktop ready

--- src/domains/rock-paper-scissors/RockPaperScissors.tsx
+++ src/domains/rock-paper-scissors/RockPaperScissors.tsx
@@ -4,13 +4,21 @@ const ActionMove = ["rock", "paper", "scissors", "lizard", "spock"];
 
 const RockPaperScissors = () => {
   return (
-    <div className="w-screen h-screen bg-gradient-to-b font-barlowSemiCondensed flex items-center justify-center font-semibold to-rpsBackgroundDark from-rpsBackgroundLight">
-      <div className="flex flex-col">
-        <div className="flex justify-between border-4 border-rpsHeader rounded-lg">
-          <div className="flex flex-col p-4">
+    <div className="w-screen h-screen justify-center bg-gradient-to-b font-barlowSemiCondensed flex font-semibold to-rpsBackgroundDark from-rpsBackgroundLight">
+      <div className="flex flex-col my-8 w-full items-center mx-4">
+        <div className="flex justify-between border-4 border-rpsHeader rounded-lg xl:rounded-2xl max-w-5xl w-full items-center p-4">
+          <div className="flex flex-col">
             {ActionMove.map((action) => (
-              <span className="text-2xl uppercase">{action}</span>
+              <span className="text-lg xl:text-3xl uppercase leading-[0.8] xl:leading-[0.8]">
+                {action}
+              </span>
             ))}
+          </div>
+          <div className="bg-white rounded-xl text-rpsScore flex flex-col items-center justify-center px-8 xl:px-12 py-3">
+            <span className="tracking-widest text-xs xl:text-xl">SCORE</span>
+            <span className="text-5xl xl:text-7xl font-bold text-rpsDark">
+              12
+            </span>
           </div>
         </div>
       </div>
