ccfc29763a9b8f3da9e3885c84c88e0dda7e01db
Daft <daftdevs@gmail.com>
2023-01-04 23:00:45 +0700

    feat: add bg color & action move header

--- src/domains/rock-paper-scissors/RockPaperScissors.tsx
+++ src/domains/rock-paper-scissors/RockPaperScissors.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-type Props = {};
+const ActionMove = ["rock", "paper", "scissors", "lizard", "spock"];
 
-const RockPaperScissors = (props: Props) => {
-  return <div>RockPaperScissors</div>;
+const RockPaperScissors = () => {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-b font-barlowSemiCondensed flex items-center justify-center font-semibold to-rpsBackgroundDark from-rpsBackgroundLight">
+      <div className="flex flex-col">
+        <div className="flex justify-between border-4 border-rpsHeader rounded-lg">
+          <div className="flex flex-col p-4">
+            {ActionMove.map((action) => (
+              <span className="text-2xl uppercase">{action}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RockPaperScissors;
