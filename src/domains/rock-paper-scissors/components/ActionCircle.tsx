14a1c95da66040bfbbc687c8e94f700d91223614
Daft <daftdevs@gmail.com>
2023-01-05 22:16:05 +0700

    style: add w-fit class

--- src/domains/rock-paper-scissors/components/ActionCircle.tsx
+++ src/domains/rock-paper-scissors/components/ActionCircle.tsx
@@ -42,7 +42,7 @@ const ActionCircle: React.FC<ActionCircleType> = ({
   return (
     <div
       {...props}
-      className={`${className} ${colorMap[action].background} ${colorMap[action].border} rounded-[100%] p-3 lg:p-4 flex items-center justify-center shadow-xl border-b-[6px]`}
+      className={`${className} ${colorMap[action].background} ${colorMap[action].border} w-fit rounded-[100%] p-3 lg:p-4 flex items-center justify-center shadow-xl border-b-[6px]`}
     >
       <div className="rounded-[100%] w-[4rem] h-[4rem] lg:w-[6rem] lg:h-[6rem] flex items-center justify-center bg-slate-100 border-t-[6px] border-gray-300">
         <img
