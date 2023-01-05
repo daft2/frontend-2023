f9c161e7686361dd3798c39e397f776421beda6e
Daft <daftdevs@gmail.com>
2023-01-05 22:16:32 +0700

    feat: action circle ui temporary

--- src/domains/rock-paper-scissors/RockPaperScissors.tsx
+++ src/domains/rock-paper-scissors/RockPaperScissors.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ActionCircle from "./components/ActionCircle";
 
 const ActionMove = ["rock", "paper", "scissors", "lizard", "spock"];
 
@@ -19,6 +20,21 @@ const RockPaperScissors = () => {
             <span className="text-5xl xl:text-7xl font-bold text-rpsDark">
               12
             </span>
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col my-40 w-[20rem] lg:w-[28rem]">
+            <div className="flex items-center justify-center">
+              <ActionCircle action="scissors" />
+            </div>
+            <div className="flex gap-32 items-center justify-center">
+              <ActionCircle action="spock" />
+              <ActionCircle action="paper" />
+            </div>
+            <div className="flex gap-8 items-center justify-center">
+              <ActionCircle action="lizard" />
+              <ActionCircle action="rock" />
+            </div>
           </div>
         </div>
       </div>
