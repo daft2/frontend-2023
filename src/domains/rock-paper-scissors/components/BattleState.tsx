f7a645a28c031c2ab3e53428bf8e5b3ce9afa38d
Daft <daftdevs@gmail.com>
2023-01-06 22:15:01 +0700

    ref: use actionmovetype & add guard in useeffect

--- src/domains/rock-paper-scissors/components/BattleState.tsx
+++ src/domains/rock-paper-scissors/components/BattleState.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { ActionMoveType } from "../RockPaperScissors";
 import ActionCircle from "./ActionCircle";
 
 type Props = {
-  playerAction: "rock" | "paper" | "scissors" | "lizard" | "spock";
+  playerAction: ActionMoveType;
 };
 
 const BattleState = (props: Props) => {
@@ -17,17 +18,19 @@ const BattleState = (props: Props) => {
   }
 
   useEffect(() => {
+    if (!props.playerAction) return;
+
     setIsLoading(true);
     setTimeout(() => {
       setEnemyMove(getEnemyMove());
       setIsLoading(false);
     }, 2000);
-  }, []);
+  }, [props.playerAction]);
 
   return (
     <div className="flex justify-between items-center my-32 w-[20rem] lg:w-[28rem]">
       <div className="flex flex-col items-center justify-between h-full gap-2">
-        <ActionCircle action="scissors" />
+        <ActionCircle action={props.playerAction} />
         <span className="tracking-wider">YOU PICKED</span>
       </div>
       <div className="flex flex-col items-center justify-between h-full gap-2">
