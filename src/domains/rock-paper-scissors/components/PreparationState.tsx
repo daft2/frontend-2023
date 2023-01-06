e141f1b01b42ffa1d5521130398ad4e26147e3a8
Daft <daftdevs@gmail.com>
2023-01-06 22:13:52 +0700

    feat: add type and select action func

--- src/domains/rock-paper-scissors/components/PreparationState.tsx
+++ src/domains/rock-paper-scissors/components/PreparationState.tsx
@@ -1,19 +1,38 @@
+import { ActionMoveType } from "../RockPaperScissors";
 import ActionCircle from "./ActionCircle";
 
-const PreparationState = () => {
+type PreparationStateType = {
+  onSelectAction: (value: ActionMoveType) => void;
+};
+
+const PreparationState: React.FC<PreparationStateType> = ({
+  onSelectAction,
+}) => {
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col my-32 w-[20rem] lg:w-[28rem]">
         <div className="flex items-center justify-center">
-          <ActionCircle action="scissors" />
+          <ActionCircle
+            action="scissors"
+            onClick={() => onSelectAction("scissors")}
+          />
         </div>
         <div className="flex gap-32 items-center justify-center">
-          <ActionCircle action="spock" />
-          <ActionCircle action="paper" />
+          <ActionCircle
+            action="spock"
+            onClick={() => onSelectAction("spock")}
+          />
+          <ActionCircle
+            action="paper"
+            onClick={() => onSelectAction("paper")}
+          />
         </div>
         <div className="flex gap-8 items-center justify-center">
-          <ActionCircle action="lizard" />
-          <ActionCircle action="rock" />
+          <ActionCircle
+            action="lizard"
+            onClick={() => onSelectAction("lizard")}
+          />
+          <ActionCircle action="rock" onClick={() => onSelectAction("rock")} />
         </div>
       </div>
     </div>
