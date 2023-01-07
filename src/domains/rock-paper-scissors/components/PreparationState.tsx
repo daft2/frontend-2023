69dfefcca2a97e07ffb30b669a93e0522f213e41
Daft <daftdevs@gmail.com>
2023-01-07 21:43:14 +0700

    ref: extract into WrappedActionCircle and add hover animation

--- src/domains/rock-paper-scissors/components/PreparationState.tsx
+++ src/domains/rock-paper-scissors/components/PreparationState.tsx
@@ -12,31 +12,39 @@ const PreparationState: React.FC<PreparationStateType> = ({
     <div className="flex items-center justify-center">
       <div className="flex flex-col my-32 w-[20rem] lg:w-[28rem]">
         <div className="flex items-center justify-center">
-          <ActionCircle
+          <WrappedActionCircle
             action="scissors"
-            onClick={() => onSelectAction("scissors")}
+            onSelectAction={onSelectAction}
           />
         </div>
         <div className="flex gap-32 items-center justify-center">
-          <ActionCircle
-            action="spock"
-            onClick={() => onSelectAction("spock")}
-          />
-          <ActionCircle
-            action="paper"
-            onClick={() => onSelectAction("paper")}
-          />
+          <WrappedActionCircle action="spock" onSelectAction={onSelectAction} />
+          <WrappedActionCircle action="paper" onSelectAction={onSelectAction} />
         </div>
         <div className="flex gap-8 items-center justify-center">
-          <ActionCircle
+          <WrappedActionCircle
             action="lizard"
-            onClick={() => onSelectAction("lizard")}
+            onSelectAction={onSelectAction}
           />
-          <ActionCircle action="rock" onClick={() => onSelectAction("rock")} />
+          <WrappedActionCircle action="rock" onSelectAction={onSelectAction} />
         </div>
       </div>
     </div>
   );
 };
+
+const WrappedActionCircle = ({
+  action,
+  onSelectAction,
+}: {
+  action: ActionMoveType;
+  onSelectAction: (value: ActionMoveType) => void;
+}) => (
+  <ActionCircle
+    action={action}
+    className="cursor-pointer hover:scale-110 ease-in-out duration-300"
+    onClick={() => onSelectAction(action)}
+  />
+);
 
 export default PreparationState;
