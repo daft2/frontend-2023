91ecd2bb5a17cc1fd8179d3cff7d9f7d461b14f2
Daft <daftdevs@gmail.com>
2023-01-16 21:18:15 +0700

    feat: add bg pattern and title

--- src/domains/ip-address-tracker/IpAddressTracker.tsx
+++ src/domains/ip-address-tracker/IpAddressTracker.tsx
@@ -3,7 +3,22 @@ import React from "react";
 type Props = {};
 
 const IpAddressTracker = (props: Props) => {
-  return <div>IpAddressTracker</div>;
+  return (
+    <div className="text-[18px] font-rubik">
+      <div className="w-full h-fit relative">
+        <img
+          src="images/ip-address-tracker/pattern-bg.png"
+          alt=""
+          className="w-full object-cover min-h-[20rem]"
+        />
+        <div className="absolute top-10 flex items-center justify-center w-full">
+          <h1 className="text-white font-medium text-2xl">
+            IP Address Tracker
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default IpAddressTracker;
